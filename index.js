6c70f66a3ecb5590c55049c469e74f84025a4e14
Collyngodlewski <collyngodlewski@gmail.com>
2021-09-13 16:00:38 -0400

    finished number one

--- index.js
+++ index.js
@@ -27,12 +27,13 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   
   Study the code for counter1 and counter2, then answer the questions below.
   
+  
   1. What is the difference between counter1 and counter2?
-  
+  counter1 has let nested inside the function and is a function scope while in counter2 the let is outside and therefore is a global variable.  
   2. Which of the two uses a closure? How can you tell?
-  
+  counter1 has a closure. the counter function is reaching for information from countermaker function
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+     counter2 be better?  counter1 would be preferable because it is resuable while counter2 would be better if the variable was used elsewhere as a global
 */
 
 // counter1 code
